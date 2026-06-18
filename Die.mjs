7c9908c7f31b6cf254b2841179d6fa0d4baa9318
Tu Nombre de Usuario <yonederzq@gmail.com>
2026-06-18 09:17:47 +0200

    Creados todos los dado

--- Die.mjs
+++ Die.mjs
@@ -1,9 +1,29 @@
 export default class Die {
-  constructor(sides = 6) {
-    this.sides = sides;
+  constructor(values) {
+    this.values = values;
   }
-  
-    roll() {
-    return Math.floor(Math.random() * this.sides) + 1;
+
+  roll() {
+    const randomIndex = Math.floor(Math.random() * this.values.length);
+    return this.values[randomIndex];
+  }
+
+  createDie3() {
+    return new Die([1, 2, 3]);
+  }
+
+  createDie5() {
+    return new Die([1, 2, 3, 4, 5]);
+  }
+
+  createDie20() {
+    return new Die([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+  }
+
+  creteDie100() {
+    for (let i = 1; i <= 100; i++) {
+      this.values.push(i);
+    }
+    return new Die(this.values);
   }
 }
--- combat.mjs
+++ combat.mjs
@@ -3,4 +3,19 @@ export default class Combat {
         this.character1 = character1;
         this.character2 = character2;
     }
+
+    start() {
+        while (this.character1.isAlive() && this.character2.isAlive()) {
+            this.character1.attack(this.character2);
+            if (this.character2.isAlive()) {
+                this.character2.attack(this.character1);
+            }
+        }
+        
+        if (this.character1.isAlive()) {
+            console.log(`${this.character1.name} wins!`);
+        } else {
+            console.log(`${this.character2.name} wins!`);
+        }
+    }
 }
